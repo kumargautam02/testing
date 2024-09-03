63edb3450df24f9a8fea2802011fe8d8551987d7
gautam <gaitbbjcdcn@gamil.com>
2024-09-03 22:32:35 +0530

    commiting from local env

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("hi1")
+    print("hi12")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
